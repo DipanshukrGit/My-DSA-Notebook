c51e6f2143550d379cd3fc9f7539a9062801e7b9
Dipanshu Kumar <dipanshuHub0411@gmail.com>
2025-06-03 15:27:45 +0530

    Add files via upload

---
+++ day 01/q1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    x = 4;
+    y = 5;
+    cout << x * y;
+    return 0;
+}
---
+++ day 01/q3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    x = 6;
+    y = 3;
+    cout << x<<"*"<< y;
+    return 0;
+}
