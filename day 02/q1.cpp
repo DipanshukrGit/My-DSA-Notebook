b1ff302b5945b8792e016f78e7783d913b5cd39d
Dipanshu Kumar <dipanshuHub0411@gmail.com>
2025-06-04 16:20:59 +0530

    Add files via upload

---
+++ day 02/q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cin>>x>>y;
+    if(x>y){
+        cout<<"the biggest number is"<<x;
+    }
+    else{
+        cout<<"the biggest number is "<<y;
+    
+    }
+}
